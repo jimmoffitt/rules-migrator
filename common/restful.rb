d05ad039078e6e9cbce882b4261298c80c47243f
Jim Moffitt <jmoffitt@twitter.com>
2016-02-26 17:15:55 -0700

    JM: adding common classes.

---
+++ common/restful.rb
@@ -0,0 +1,121 @@
+class GnipRESTful
+    require "net/https"     #HTTP gem.
+    require "uri"
+
+    attr_accessor :url, :uri, 
+				  :user_name, 
+				  :password, 
+				  :headers, 
+				  :data
+
+    def initialize(url=nil, user_name=nil, password=nil, headers=nil)
+	   @url = url if not url.nil?
+	   @uri = URI.parse(@url) if not @url.nil?
+	   
+	   @user_name = user_name if not user_name.nil?
+	   @password = password if not password.nil?
+	   @headers = headers if not headers.nil?
+    end
+
+    def url=(value)
+        @url = value
+        @uri = URI.parse(@url)
+    end
+
+    #Fundamental REST API methods:
+    def POST(url = nil, data = nil)
+
+  	    @data = data
+		@url = url if not url.nil?
+
+        uri = URI(@url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.path)
+        request.body = @data
+        request.basic_auth(@user_name, @password)
+
+        begin
+            response = http.request(request)
+        rescue
+            logger()
+            sleep 5
+            response = http.request(request) #try again
+        end
+
+        return response
+    end
+
+    def PUT(url = nil, data = nil)
+
+	   @data = data if not data.nil? #if request data passed in, use it.
+	   @url = url if not url.nil?
+
+
+        uri = URI(@url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Put.new(uri.path)
+        request.body = @data
+        request.basic_auth(@user_name, @password)
+
+        begin
+            response = http.request(request)
+        rescue
+            sleep 5
+            response = http.request(request) #try again
+        end
+
+        return response
+    end
+
+    def GET(url = nil, params = nil)
+	   
+	    @url = url if not url.nil?
+	   
+        uri = URI(@url)
+
+        #params are passed in as a hash.
+        #Example: params["max"] = 100, params["since_date"] = 20130321000000
+        if not params.nil?
+            uri.query = URI.encode_www_form(params)
+        end
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request.basic_auth(@user_name, @password)
+
+        begin
+            response = http.request(request)
+        rescue
+            sleep 5
+            response = http.request(request) #try again
+        end
+
+        return response
+    end
+
+    def DELETE(url = nil, data = nil)
+        if not data.nil?
+            @data = data
+        end
+
+        uri = URI(@url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Delete.new(uri.path)
+        request.body = @data
+        request.basic_auth(@user_name, @password)
+
+        begin
+            response = http.request(request)
+        rescue
+            sleep 5
+            response = http.request(request) #try again
+        end
+
+        return response
+    end
+end #PtREST class.
+
