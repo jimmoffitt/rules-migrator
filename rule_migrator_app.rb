bd4345ba4c34b5d01237383847c0647a8426995a
Jim Moffitt <jmoffitt@twitter.com>
2016-03-18 13:17:01 -0600

    JM: nearly a complete rewrite...

--- rule_migrator_app.rb
+++ rule_migrator_app.rb
@@ -1,69 +1,98 @@
 require 'optparse'
 
-require_relative './common/app_logger'
+require_relative './common/app/app_logger'
 require_relative './lib/rules_migrator'
+
+def parseOptions(o)
+
+   settings = {}
+
+   settings['account'] = nil
+   settings['config'] = nil
+   settings['target'] = nil
+   settings['source'] = nil
+   settings['verbose'] = nil
+
+   #Passing in a config file.... Or you can set a bunch of parameters.
+   o.on('-a ACCOUNT', '--account', 'Account configuration file (including path) that provides OAuth settings.') { |account| settings['account']= account }
+   o.on('-c CONFIG', '--config', 'Settings configuration file (including path) that provides API settings.') { |config| settings['config'] = config }
+   o.on('-s SOURCE', '--source', '') { |source| settings['source'] = source }
+   o.on('-t TARGET', '--target', '') { |target| settings['target'] = target }
+   o.on('-v', '--verbose', 'When verbose, output all kinds of things, each request, most responses, etc.') { |verbose| $verbose = verbose }
+   o.on('-h', '--help', 'Display this screen.') do
+	  puts o #Help screen.
+	  exit
+   end
+   o.parse!
+
+   settings
+
+end
+
+def set_defaults settings
+
+   #If not passed in, use some defaults.
+   settings['account'] = "../config/account.yaml" if settings['account'].nil?
+   settings['config'] = '../config/options.yaml' if settings['config'].nil?
+   settings['verbose'] = true if settings['verbose'].nil?
+
+   settings
+end
+
 
 if __FILE__ == $0 #This script code is executed when running this file.
 
-   OptionParser.new do |o|
+   settings = {}
 
-	  #Passing in a config file.... Or you can set a bunch of parameters.
-	  o.on('-a ACCOUNT', '--account', 'Account configuration file (including path) that provides OAuth settings.') { |account| $account = account }
-	  o.on('-c CONFIG', '--config', 'Settings configuration file (including path) that provides API settings.') { |settings| $settings = settings }
-	  o.on('-s SOURCE', '--source', '') { |source| $source = source }
-	  o.on('-t TARGET', '--target', '') { |target| $target = target }
-	  o.on('-v', '--verbose', 'When verbose, output all kinds of things, each request, most responses, etc.') { |verbose| $verbose = verbose }
-	  o.on('-h', '--help', 'Display this screen.') do
-		 puts o #Help screen.
-		 exit
-	  end
-	  o.parse!
+   OptionParser.new do |o|
+	  settings = parseOptions o
    end
 
-   #If not passed in, use some defaults.
-   $account = "./config/account.yaml" if $account.nil?
-   $settings = './config/options.yaml' if ($settings.nil?) 
+   settings = set_defaults settings
 
-   AppLogger.set_logger($settings, $verbose)
+   AppLogger.set_logger(settings['config'], settings['verbose'])
    AppLogger.log_info("Starting process at #{Time.now}")
-   Migrator = RulesMigrator.new($account, $settings, $verbose)
+   Migrator = RulesMigrator.new(settings['account'], settings['config'], settings['verbose'])
 
    #Set application attributes from command-line. These override values in the configuration file.
 
    #---------------------------------------------------------------------------------------------------------------------
 
-   
-   continue = Migrator.check_systems(Migrator.source,Migrator.target)
-   
-   Migrator.target[:rules] = Migrator.get_rules(Migrator.target)
-   if Migrator.target[:rules].nil?
-	  AppLogger.log_error 'Problem checking target rules.'
-   else
-	  Migrator.target[:num_rules_before] = Migrator.target[:rules].count
-   end
-      
-   Migrator.source[:rules] = Migrator.get_rules(Migrator.source)
-   if Migrator.source[:rules].nil?
-	  AppLogger.log_error 'Problem checking source rules.'
-   else
-	  Migrator.source[:num_rules] = Migrator.source[:rules].count
-	  continue = Migrator.post_rules(Migrator.source, Migrator.target)
-   end
-  
-   if continue
-	  Migrator.target[:rules] = Migrator.get_rules(Migrator.target)
-	  if Migrator.target[:rules].nil?
-		 AppLogger.log_error 'Problem re-checking target rules.'
-		 continue = false
-	  else
-		 Migrator.target[:num_rules_after] = Migrator.target[:rules].count
-	  end
-	end
+   #Only fail here is down grading (target_version >= source_target). 
+   continue = Migrator.check_systems(Migrator.source[:url], Migrator.target[:url]) 
 
-	if continue
-	  Migrator.write_summary
+   if continue 
+
+	  #Load Target rules.
+	 Migrator.target[:rules] = Migrator.load_rules(Migrator.target)
+	 continue = false if Migrator.target[:rules].nil?
+	 
+	 #Load Source rules.
+	 Migrator.source[:rules] = Migrator.load_rules(Migrator.source) if continue
+	 continue = false if Migrator.target[:rules].nil?
+	 Migrator.source[:num_rules] = Migrator.source[:rules].count #TODO: Why set here?
+
+     #Do translation if going 'up-version' (PT 1.0 --> 2.0).
+	 Migrator.target[:rules] = Migrator.translate_rules(Migrator.source[:rules]) if Migrator.do_rule_translation
+	 continue = false if Migrator.target[:rules].nil?
+	 
+	 #Migrate (Source) rules to Target system.
+	 continue = Migrator.post_rules(Migrator.target)
+
+     #If migration success, recheck Target rules. 	 
+	  if continue
+		 Migrator.target[:rules] = Migrator.load_rules(Migrator.target)
+		 AppLogger.log_error "ERROR re-checking target rules." if Migrator.target[:rules].nil?
+         Migrator.target[:num_rules_after] = Migrator.target[:rules].count
+	  end
+
+	  if continue
+		 Migrator.write_summary 
+	  else
+		 AppLogger.log_error 'Did not post rules to Target.'
+	  end
    else
-	  AppLogger.log_error 'Did not post rules to Target.'
+	  AppLogger.log_error "Can not migrate PowerTrack 2.0 rules to PowerTrack 1.0 stream."
    end
 
    #------------------------------------------------------------------
