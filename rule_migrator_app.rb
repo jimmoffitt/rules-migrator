45bbbf22a5eff787db1b30ebce4b7662fcb77c8a
Jim Moffitt <jmoffitt@twitter.com>
2016-04-12 15:31:02 -0600

    JM: more details.

--- rule_migrator_app.rb
+++ rule_migrator_app.rb
@@ -59,6 +59,8 @@ if __FILE__ == $0 #This script code is executed when running this file.
    AppLogger.set_logger(settings['config'], settings['verbose'])
    AppLogger.log_info("Starting process at #{Time.now}")
    Migrator = RulesMigrator.new(settings['account'], settings['config'], settings['verbose'])
+   AppLogger.verbose = Migrator.options[:verbose]
+   AppLogger.info_chatter = true #Even if verbose is false, let's treat info log messages in a verbose way...
 
    #Set application attributes from command-line. These override values in the configuration file.
 
@@ -75,19 +77,24 @@ if __FILE__ == $0 #This script code is executed when running this file.
 	 
 	 #Load Source rules.
 	 Migrator.source[:rules] = Migrator.load_rules(Migrator.source) if continue
-	 continue = false if Migrator.target[:rules].nil?
+	 continue = false if Migrator.source[:rules].nil?
 	 Migrator.source[:num_rules] = Migrator.source[:rules].count #TODO: Why set here?
 
      #Do translation if going 'up-version' (PT 1.0 --> 2.0).
-	 Migrator.target[:rules] = Migrator.translate_rules(Migrator.source[:rules]) if Migrator.do_rule_translation
-	 continue = false if Migrator.target[:rules].nil?
+	 if Migrator.do_rule_translation
+	    Migrator.target[:rules] = Migrator.translate_rules(Migrator.source[:rules])
+	 else
+		Migrator.target[:rules] = Migrator.source[:rules]
+	 end
+
+     continue = false if Migrator.target[:rules].nil?
 	 
 	 #Migrate (Source) rules to Target system.
 	 continue = Migrator.post_rules(Migrator.target)
 
      #If migration success, recheck Target rules. 	 
 	  if continue
-		 Migrator.target[:rules] = Migrator.load_rules(Migrator.target)
+		 Migrator.target[:rules] = Migrator.load_rules(Migrator.target, false)
 		 AppLogger.log_error "ERROR re-checking target rules." if Migrator.target[:rules].nil?
          Migrator.target[:num_rules_after] = Migrator.target[:rules].count
 	  end
