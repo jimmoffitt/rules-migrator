ae2f07ed1ff922e215c567cc0c96f7b541f6c3a3
Jim Moffitt <jmoffitt@twitter.com>
2016-08-05 17:03:26 -0600

    JM: updating handling of headers.

--- lib/common/restful.rb
+++ lib/common/restful.rb
@@ -23,23 +23,44 @@ class GnipRESTful
     end
 
     #Fundamental REST API methods:
-    def POST(url = nil, data = nil)
+    def POST(url = nil, data = nil, headers = nil, parameters = nil)
 
   	    @data = data
 		@url = url if not url.nil?
-
+				
         uri = URI(@url)
+
+		#params are passed in as a hash.
+		#Example: params["max"] = 100, params["since_date"] = 20130321000000
+		if not parameters.nil?
+		   uri.query = URI.encode_www_form(parameters)
+		end
+				
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
-        request = Net::HTTP::Post.new(uri.path)
+
+		#TODO: port to DELETE method with new optional method parameter that does a POST
+		if !parameters.nil? and parameters.key?("_method") 
+			request = Net::HTTP::Post.new(uri.path + "?_method=delete")
+		else
+		   request = Net::HTTP::Post.new(uri.path)
+		end
+
+		#Add headers
+		if not headers.nil?
+			headers.each do |header|
+		   		request[header[0]] = header[1]
+			end
+		end
+
         request.body = @data
         request.basic_auth(@user_name, @password)
 
         begin
             response = http.request(request)
         rescue
-            logger()
             sleep 5
+			puts response
             response = http.request(request) #try again
         end
 
